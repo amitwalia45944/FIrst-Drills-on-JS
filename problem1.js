c083c89a267984c8abbd3554d05fcb3c9f1024b8
amit <aimldl45944@gmail.com>
2023-07-12 12:36:39 +0530

    completed problem1

---
+++ problem1.js
@@ -0,0 +1,12 @@
+function problem1(inventory)
+{
+    for(let i=0;i<inventory.length;i++)
+    {
+        if(inventory[i].id===33)
+        {
+            return inventory[i];
+        }
+    }
+}
+
+module.exports = problem1;
